a6e260fda1012a755ce0f7050340f5f5c48c9ebf
DanielaArgumanis <daniela.argumanis@gmail.com>
2024-03-24 19:36:52 -0500

    :wastebasket: Cleaning : Clean imports

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 import { QRCode } from 'react-qrcode-logo';
 import OptionsForm from './OptionsForm';
 import Typography from '@commonComponents/Typography/Typography';
-import { TextInputSize } from '@commonComponents/inputs/InputTypes';
 
 // @Icons
 import { ChevronIcon, CopyIcon, DownloadIcon } from '@icons';
@@ -22,6 +21,7 @@ import {
 } from './Home.styles';
 
 // @Types
+import { TextInputSize } from '@commonComponents/inputs/InputTypes';
 import { QRCodeOptions, QRForm } from './Home.types';
 import { QRGeneratorStorage } from '@services/google/googleServices.types';
 
@@ -147,11 +147,9 @@ const Home = () => {
         </Button>
       </ButtonsContainer>
       {message && (
-        <div>
-          <Typography variant="body3" margin="0 0 1 0">
-            {message}
-          </Typography>
-        </div>
+        <Typography variant="body3" margin="0 0 1 0">
+          {message}
+        </Typography>
       )}
     </HomeContainer>
   );
